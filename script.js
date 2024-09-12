96bab6047b9fe66a040b909462bdbc8e692c91e7
nextor3798 <130645255+nextor3798@users.noreply.github.com>
2024-09-12 09:05:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('header').addEventListener('mouseover', function() {
+  this.style.backgroundColor = "#3e8e41";
+});
+document.querySelector('header').addEventListener('mouseout', function() {
+  this.style.backgroundColor = "#f1f1f1";
+});
+document.querySelector('header').addEventListener('click', function() {
+  this.style.backgroundColor = "#f1f1f1";
+});
